d9f6515379fd0a8895efaa183382e8a575f37190
Giacomo Petrillo <info@giacomopetrillo.com>
2024-03-14 22:13:24 -0500

    fixes in the example

--- examples/example.py
+++ examples/example.py
@@ -9,10 +9,10 @@ warnings.filterwarnings('error', r'scatter inputs have incompatible types.*', Fu
 
 # DGP config
 n = 200 # number of datapoints (train + test)
-p = 10 # number of covariates
+p = 1 # number of covariates
 sigma = 0.1 # noise standard deviation
 def f(x): # conditional mean
-    R = 0.5
+    R = 2
     r2 = jnp.einsum('ij,ij->j', x, x)
     return jnp.cos(2 * jnp.pi / R * jnp.sqrt(r2))
 
@@ -32,20 +32,28 @@ X_test = X_train
 y_test = y_train
 
 # fit with bartz
-bart = bartz.BART(X_train, y_train, x_test=X_test)
+bart = bartz.BART(X_train, y_train, x_test=X_test, ntree=200, nskip=500, ndpost=500, seed=91287346)
 
 # compute RMSE
 resid = y_test - bart.yhat_test_mean
-rmse = jnp.sqrt(resid @ resid)
+rmse = jnp.sqrt(resid @ resid / resid.size)
+print(f'data sdev = {y_test.std():#.2g}')
 print(f'sigma: {bart.sigma.mean():#.2g} (true: {sigma:#.2g})')
 print(f'RMSE: {rmse.item():#.2g}')
 
 # plot true vs. predicted
-fig, ax = plt.subplots(num='example', clear=True)
+fig, axs = plt.subplots(1, 2, num='example', clear=True, figsize=[10, 5])
+
+ax = axs[0]
 l = min(bart.yhat_test_mean.min(), y_test.min())
 h = max(bart.yhat_test_mean.max(), y_test.max())
 ax.plot([l, h], [l, h], color='lightgray')
 ax.plot(bart.yhat_test_mean, y_test, '.')
 ax.set(xlabel='predicted (post mean)', ylabel='true', title='true vs. predicted on test set')
+
+ax = axs[1]
+ax.plot(X_test[0], y_test, '.', label='data')
+ax.plot(X_test[0], bart.yhat_test_mean, 'o', label='pred', markerfacecolor='none')
+ax.legend()
 
 fig.show()
