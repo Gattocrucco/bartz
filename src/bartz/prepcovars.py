fix splits calculation

src/bartz/prepcovars.py
@@ -60,10 +60,10 @@ def quantilized_splits_from_matrix_impl(x, out_length):
     u = jnp.unique(x, size=x.size, fill_value=huge)
     actual_length = jnp.count_nonzero(u < huge) - 1
     midpoints = (u[1:] + u[:-1]) / 2
-    indices = jnp.arange(out_length) * (actual_length - 1) // (out_length - 1) # <-- potential integer overflow with int32!
-        # TODO: this indices expression always includes the first and last
-        # elements, I should make it longer by 2 and then exclude the
-        # extremities. Or not? Or at the level of x rather than the midpoints?
+    indices = jnp.linspace(-1, actual_length, out_length + 2)[1:-1]
+    indices = jnp.around(indices).astype(int)
+        # indices calculation with float rather than int to avoid potential
+        # overflow with int32, and to round to nearest instead of rounding down
     decimated_midpoints = midpoints[indices]
     truncated_midpoints = midpoints[:out_length]
     splits = jnp.where(actual_length > out_length, decimated_midpoints, truncated_midpoints)