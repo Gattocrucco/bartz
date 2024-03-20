unit tests; fixed scaling issues

src/bartz/interface.py
@@ -108,6 +108,8 @@ class BART:
         The prior standard deviation of the latent mean function.
     lamda : float
         The prior harmonic mean of the error variance.
+    sigest : float or None
+        The estimated standard deviation of the error used to set `lamda`.
     ntree : int
         The number of trees.
     yhat_train : array (ndpost, n)
@@ -166,17 +168,17 @@ class BART:
         y_train, y_train_fmt = self._process_response_input(y_train)
         self._check_same_length(x_train, y_train)
         
-        lamda = self._process_noise_variance_settings(x_train, y_train, sigest, sigdf, sigquant, lamda)
         offset = self._process_offset_settings(y_train, offset)
         scale = self._process_scale_settings(y_train, k)
+        lamda, sigest = self._process_noise_variance_settings(x_train, y_train, sigest, sigdf, sigquant, lamda, offset)
 
         splits, max_split = self._determine_splits(x_train, numcut)
         x_train = self._bin_predictors(x_train, splits)
 
         y_train = self._transform_input(y_train, offset, scale)
-        lamda = lamda / scale
+        lamda_scaled = lamda / (scale * scale)
 
-        mcmc_state = self._setup_mcmc(x_train, y_train, max_split, lamda, sigdf, power, base, maxdepth, ntree)
+        mcmc_state = self._setup_mcmc(x_train, y_train, max_split, lamda_scaled, sigdf, power, base, maxdepth, ntree)
         final_state, burnin_trace, main_trace = self._run_mcmc(mcmc_state, ndpost, nskip, keepevery, printevery, seed)
 
         sigma = self._extract_sigma(main_trace, scale)
@@ -184,7 +186,8 @@ class BART:
 
         self.offset = offset
         self.scale = scale
-        self.lamda = lamda * scale
+        self.lamda = lamda
+        self.sigest = sigest
         self.ntree = ntree
         self.sigma = sigma
         self.first_sigma = first_sigma
@@ -261,25 +264,25 @@ class BART:
         assert get_length(x1) == get_length(x2)
 
     @staticmethod
-    def _process_noise_variance_settings(x_train, y_train, sigest, sigdf, sigquant, lamda):
+    def _process_noise_variance_settings(x_train, y_train, sigest, sigdf, sigquant, lamda, offset):
         if lamda is not None:
-            return lamda
+            return lamda, None
         else:
             if sigest is not None:
                 sigest2 = sigest * sigest
             elif y_train.size < 2:
                 sigest2 = 1
             elif y_train.size <= x_train.shape[0]:
-                sigest2 = jnp.var(y_train)
+                sigest2 = jnp.var(y_train - offset)
             else:
-                _, chisq, rank, _ = jnp.linalg.lstsq(x_train.T, y_train)
+                _, chisq, rank, _ = jnp.linalg.lstsq(x_train.T, y_train - offset)
                 chisq = chisq.squeeze(0)
                 dof = len(y_train) - rank
                 sigest2 = chisq / dof
             alpha = sigdf / 2
             invchi2 = jaxext.scipy.stats.invgamma.ppf(sigquant, alpha) / 2
             invchi2rid = invchi2 * sigdf
-            return sigest2 / invchi2rid
+            return sigest2 / invchi2rid, jnp.sqrt(sigest2)
 
     @staticmethod
     def _process_offset_settings(y_train, offset):

tests/test_interface.py
@@ -22,39 +22,101 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import pytest
 from jax import numpy as jnp
 from jax import random
+import numpy
 
 import bartz
 
-def test_basic(key):
-    
-    # DGP config
-    n = 30 # number of datapoints
-    p = 10 # number of covariates
-    sigma = 0.1 # noise standard deviation
-    def f(x): # conditional mean
-        T = 2
-        return jnp.sum(jnp.cos(2 * jnp.pi / T * x), axis=0)
-    def gen_X(key, p, n): # predictors
-        return random.uniform(key, (p, n), float, -2, 2)
+@pytest.fixture
+def n():
+    return 30
 
-    # random keys
-    key1, key2, key3 = random.split(key, 3)
+@pytest.fixture
+def p():
+    return 2
 
-    # generate data
-    X = gen_X(key1, p, 2 * n)
-    y = f(X) + sigma * random.normal(key2, (2 * n,))
+def gen_X(key, p, n):
+    return random.uniform(key, (p, n), float, -2, 2)
 
-    # split in train/test
-    X_train, X_test = X[:, :n], X[:, n:]
-    y_train, y_test = y[:n], y[n:]
+@pytest.fixture
+def X(n, p, key):
+    key = random.fold_in(key, 0xd9b0963d)
+    return gen_X(key, p, n)
 
-    # fit with bartz
-    bart = bartz.BART(X_train, y_train, x_test=X_test, seed=key3)
+def f(x): # conditional mean
+    T = 2
+    return jnp.sum(jnp.cos(2 * jnp.pi / T * x), axis=0)
 
+@pytest.fixture
+def y(X, key):
+    sigma = 0.1
+    key = random.fold_in(key, 0x1391bc96)
+    return f(X) + sigma * random.normal(key, (X.shape[1],))
+
+@pytest.fixture
+def kw():
+    return dict(ntree=20, ndpost=100, nskip=100)
+
+def test_bad_trees(X, y, key, kw):
+    bart = bartz.BART(X, y, **kw, seed=key)
     bad = bart._check_trees()
     bad_count = jnp.count_nonzero(bad)
     assert bad_count == 0
 
-# I should share n, p, and various integers between as many tests as possible to reduce compilation times.
+def test_sequential_guarantee(X, y, key, kw):
+    bart1 = bartz.BART(X, y, **kw, seed=key)
+    
+    kw['nskip'] -= 1
+    kw['ndpost'] += 1
+    bart2 = bartz.BART(X, y, **kw, seed=key)
+    
+    numpy.testing.assert_array_equal(bart1.yhat_train, bart2.yhat_train[1:])
+
+    kw['keepevery'] = 2
+    bart3 = bartz.BART(X, y, **kw, seed=key)
+    yhat_train = bart2.yhat_train[1::2]
+    numpy.testing.assert_array_equal(yhat_train, bart3.yhat_train[:len(yhat_train)])
+
+def test_finite(X, y, key, kw):
+    bart = bartz.BART(X, y, **kw, seed=key)
+    assert jnp.all(jnp.isfinite(bart.yhat_train))
+
+def test_output_shapes(X, y, key, kw):
+    bart = bartz.BART(X, y, x_test=X, **kw, seed=key)
+
+    ndpost = kw['ndpost']
+    nskip = kw['nskip']
+    _, n = X.shape
+
+    assert bart.offset.shape == ()
+    assert bart.scale.shape == ()
+    assert bart.lamda.shape == ()
+    assert bart.yhat_train.shape == (ndpost, n)
+    assert bart.yhat_test.shape == (ndpost, n)
+    assert bart.yhat_train_mean.shape == (n,)
+    assert bart.yhat_test_mean.shape == (n,)
+    assert bart.sigma.shape == (ndpost,)
+    assert bart.first_sigma.shape == (nskip,)
+
+def test_predict(X, y, key, kw):
+    bart = bartz.BART(X, y, **kw, seed=key)
+    yhat_train = bart.predict(X)
+    numpy.testing.assert_array_equal(bart.yhat_train, yhat_train)
+
+def test_scale_shift(X, y, key, kw):
+    bart1 = bartz.BART(X, y, **kw, seed=key)
+
+    offset = 0.4703189
+    scale = 0.5294714
+    bart2 = bartz.BART(X, offset + y * scale, **kw, seed=key)
+
+    numpy.testing.assert_allclose(bart1.offset, (bart2.offset - offset) / scale, rtol=1e-6)
+    numpy.testing.assert_allclose(bart1.scale, bart2.scale / scale)
+    numpy.testing.assert_allclose(bart1.sigest, bart2.sigest / scale)
+    numpy.testing.assert_allclose(bart1.lamda, bart2.lamda / scale ** 2)
+    numpy.testing.assert_allclose(bart1.yhat_train, (bart2.yhat_train - offset) / scale, atol=1e-5, rtol=1e-5)
+    numpy.testing.assert_allclose(bart1.yhat_train_mean, (bart2.yhat_train_mean - offset) / scale, atol=1e-5, rtol=1e-5)
+    numpy.testing.assert_allclose(bart1.sigma, bart2.sigma / scale, atol=1e-6, rtol=1e-6)
+    numpy.testing.assert_allclose(bart1.first_sigma, bart2.first_sigma / scale, atol=1e-6, rtol=1e-6)