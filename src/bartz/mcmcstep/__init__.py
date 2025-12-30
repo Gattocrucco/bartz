77de304d8ebd215f74af1932e5824ad5c4307630
Gattocrucco <info@giacomopetrillo.com>
2025-12-30 11:31:15 +0100

    refactor: split mcmcstep, not tested

---
+++ src/bartz/mcmcstep/__init__.py
@@ -0,0 +1,42 @@
+# bartz/src/bartz/mcmcstep/__init__.py
+#
+# Copyright (c) 2025, The Bartz Contributors
+#
+# This file is part of bartz.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Functions that implement the BART posterior MCMC initialization and update step.
+
+Functions that do MCMC steps operate by taking as input a bart state, and
+outputting a new state. The inputs are not modified.
+
+The entry points are:
+
+  - `State`: The dataclass that represents a BART MCMC state.
+  - `init`: Creates an initial `State` from data and configurations.
+  - `step`: Performs one full MCMC step on a `State`, returning a new `State`.
+  - `step_sparse`: Performs the MCMC update for variable selection, which is skipped in `step`.
+"""
+
+# ruff: noqa: F401
+
+from bartz.mcmcstep._state import Forest, State, init
+from bartz.mcmcstep._step import step, step_sparse
